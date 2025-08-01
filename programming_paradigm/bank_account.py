06fc03c83a219d7b1d2b94400b0bd3a4b42d8147
precious thomas <preshusthomas28@gmail.com>
2025-08-01 20:46:48 +0100

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,19 +1,16 @@
 class BankAccount:
     def __init__(self, initial_balance=0):
-        self.account_balance = float(initial_balance)  
+        self.account_balance = float(initial_balance)
+
     def deposit(self, amount):
-            if amount > 0:
-                self.balance += amount
-                self.transaction_history.append(f"Deposited: ${amount}")
-            else:
-                print("invalid deposit amount.")
+        self.account_balance += amount
+
     def withdraw(self, amount):
-            if 0 < amount <= self.balance:
-                self.balance -= amount
-                self.transaction_history.append(f"Withdrew: ${amount}")
-                return True
-            else:
-                print("Insufficient funds or invalid withdrawal amount.")
-                return False
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
     def display_balance(self):
-            print(f"Current Balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
